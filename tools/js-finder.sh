598640f96e7b7907f0011582f873f135c40fb7e8
syrine0x01 <avyukt.syrine@gmail.com>
2023-01-12 13:38:50 +0530

    new code js-finder

--- tools/js-finder.sh
+++ tools/js-finder.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-if [[ "$1" == "" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]
+echo ""
+figlet -f small "JS finder"
+echo ""
+
+if [[ $1 == ""  ]] || [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
 then
 	echo ""
-	echo "$ bash js-finder.sh {file containing target urls}"
+	echo "$ bash js-finder.sh {file containing urls}"
 	echo ""
-	echo "requirements: "
-	exit
+	echo "requirements: figlet, waybackurls, gau, anew, nuclei"
+	echo ""
+	echo "output: js.txt | nuclei_js.txt"
 fi
+
 
 file=$1
 
-cat $file | gauplus | getJS --complete
+rm -rf js.txt
+rm -rf nuclei_js.txt
+
+echo ""
+echo "[+] searching for js files..."
+echo "" 
+
+cat $file | { waybackurls; gau; } | httpx -silent -timeout=30 -content-type | grep -E "text/javascript|application/javascript" | cut -d " " -f1 | anew js.txt
+
+echo ""
+echo "[+] js file all fetched | output : js.txt"
+echo ""
+echo "[+] nuclei scan checking for confidential information in js files..."
+echo ""
+
+nuclei -l js.txt -es info | anew nuclei_js.txt
+
+echo ""
+echo "[+] nuclei scan completed | output : nuclei_js.txt"
+echo ""
